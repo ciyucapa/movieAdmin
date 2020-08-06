385a16866197d4c0c60072629e1634e7d34efc32
ciyucapa <cindycaceres134@gmail.com>
2020-08-05 22:38:38 -0500

    login con validacio

--- src/scenes/auth/login/index.js
+++ src/scenes/auth/login/index.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 
 const Login = () => {
 
-    const [email, setEmail] = useState("");
+    const [email, setEmail] = useState('');
     const handleEmailChange = (event) => {
         setEmail(event.target.value)
     };
@@ -20,12 +20,17 @@ const Login = () => {
         backgroundColor: "#33FFF0" 
     };
 
+    const isEmail = () => {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+    }
+
     return(
         <div>
             <form>
                     Email <input type="email" value={email} onChange={handleEmailChange} />
                     Contraseña <input type="text" value={password} onChange={handlePasswordChange} />
-                    <button style={ email && password ? style2 : style }> Enviar</button>
+                    <button style={ email && password && isEmail() ? style2 : style }> Enviar</button>
             </form>
         </div>
     );
