67d21d3f37d7aeb12e8cd332c4424261bca6267e
ciyucapa <cindycaceres134@gmail.com>
2020-08-10 15:30:18 -0500

    cambios

--- src/scenes/auth/register/index.js
+++ src/scenes/auth/register/index.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useState, useEffect} from 'react';
+import React, {useReducer} from 'react';
 import '../login/index.css';
 
 const initialState = {
@@ -11,10 +11,68 @@ const initialState = {
     cop: 0,
 };
 
+const fee = 3600;
+const AMOUNT_CHANGE = 'AMOUNT_CHANGE';
+const FIRST_NAME_CHANGE = 'FIRST_NAME_CHANGE';
+const LAST_NAME_CHANGE = 'LAST_NAME_CHANGE';
+const EMAIL_CHANGE = 'EMAIL_CHANGE';
+const PASSWORD_CHANGE = 'PASSWORD_CHANGE';
+const PASSWORD_CONFIRM_CHANGE = 'PASSWORD_CONFIRM_CHANGE';
+
 const reducer = (state, action) => {
+    const {type} = action;
 
+    switch (type) {
+        case AMOUNT_CHANGE: {
+            const {payload} = action;
+            return {
+                ...state,
+                usd: payload,
+                cop: payload * fee
+            }
+        }
+        case FIRST_NAME_CHANGE: {
+            const {payload} = action;
+            return {
+                ...state,
+                firstName: payload,
+            }
+        }
+        case LAST_NAME_CHANGE: {
+            const {payload} = action;
+            return {
+                ...state,
+                lastName: payload,
+            }
+        }
+        case EMAIL_CHANGE: {
+            const {payload} = action;
+            return {
+                ...state,
+                email: payload,
+            }
+        }
+        case PASSWORD_CHANGE: {
+            const {payload} = action;
+            return {
+                ...state,
+                password: payload,
+            }
+        }
+        case PASSWORD_CONFIRM_CHANGE: {
+            const {payload} = action
+            return {
+                ...state,
+                passwordConfirm: payload,
+            }
+        }
+        default: {
+            return{
+                ...state
+            }
+        }
+    };
 };
-
 const Register = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const {firstName, lastName, email, password, passwordConfirm, usd, cop} = state;
@@ -22,6 +80,43 @@ const Register = () => {
     const amountHandler = (event) => {
         const value = parseFloat(event.target.value);
         console.log('El monto del input es de: ', value);
+        dispatch({
+            type: AMOUNT_CHANGE,
+            payload: value,
+        });
+    };
+
+    const firstNameChange = (event) => {
+        dispatch({
+            type: FIRST_NAME_CHANGE,
+            payload: event.target.value,
+        });
+    };
+
+    const lastNameChange = (event) => {
+        dispatch({
+            type: LAST_NAME_CHANGE,
+            payload: event.target.value,
+        });
+    };
+
+    const emailChange = (event) => {
+        dispatch ({
+            type: EMAIL_CHANGE,
+            payload: event.target.value,
+        })
+    };
+    const passwordChange = (event) => {
+        dispatch ({
+            type: PASSWORD_CHANGE,
+            payload: event.target.value,
+        })
+    };
+    const passwordConfirmChange = (event) => {
+        dispatch({
+            type: PASSWORD_CONFIRM_CHANGE,
+            payload: event.target.value,
+        })
     };
 
     return(
@@ -29,24 +124,24 @@ const Register = () => {
             <div className="container-form"> 
                 <form>
                     <h1>Registro de Usuario</h1>
-                    <label>Nombre
-                    <input className="input" name="nombre" type="text" value={firstName} />
+                    <label>First Name
+                    <input className="input" name="firstName" type="text" value={firstName} onChange={firstNameChange} />
                     </label>
-                    <label>Apellido
-                    <input className="input" name="apellido" type="text" value={lastName} />
+                    <label>LastName
+                    <input className="input" name="lastName" type="text" value={lastName} onChange={lastNameChange} />
                     </label>
-                    <label>Correo electronico
-                    <input className="input" name="correo" type="email" value={email} />
+                    <label>Email
+                    <input className="input" name="email" type="email" value={email} onChange={emailChange} />
                     </label>
-                    <label>Contraseña
-                    <input className="input" name="password" type="password" value={password} />
+                    <label>Password
+                    <input className="input" name="password" type="password" value={password} onChange={passwordChange} />
                     </label>
-                    <label>Repetir contraseña
-                    <input className="input" name="repeat-password" type="password" value={passwordConfirm} />
+                    <label>Password Confirm
+                    <input className="input" name="passwordConfirm" type="password" value={passwordConfirm} onChange={passwordConfirmChange} />
                     </label>
                     El monto es de: {usd} USD
                     que equivale a: {cop} COP
-                    <input className="input" name="Monto" type="number" value={usd} onChange={amountHandler} />
+                    <input className="input" name="amount" type="number" value={usd} onChange={amountHandler} />
                     <button 
                     type="submit">Enviar!</button>
                 </form>
